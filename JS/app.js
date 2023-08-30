b96e05aa6d981359623ef2e63fad33e0bf4117c3
Josefina PM <josefina.perez.mercader@gmail.com>
2023-08-29 21:49:29 -0300

    modifications: include array objects + methods

--- JS/app.js
+++ JS/app.js
@@ -2,89 +2,75 @@ let Name = prompt("Please write your name");
 if (Name === "") {
     alert("Please write your Name");
 } else {
-    alert("Your name is " + Name)
+console.log(Name)
 }
 
 let LastName = prompt('Please write your Last Name');
 if (LastName === "") {
     alert("Please write your Last Name");
 } else {
-    alert("Your last name is " + LastName)
+    console.log(LastName)
 }
 
 let Email = prompt('Please write your Email');
 if (Email === "") {
     alert("Please write your Email");
 } else {
-    alert("Your email is " + Email)
+    console.log(Email)
 }
 
-function Quote (Services, Region, pricing)
-{
-    this.Services= Services;
-    this.Region=Region;
-    this.pricing=pricing;
-}
 
 let userServices = prompt('Please write Services required. Options are: Data Analysis Projects, Visualization Projects, Prediction models, Training and workshops or Full-day training');
 
 while (userServices === "" || !['data analysis projects', 'visualization projects', 'prediction models', 'training and workshops', 'full-day training'].includes(userServices.toLowerCase())) {
     userServices = prompt('Please write a correct Service. Options are: Data Analysis Projects, Visualization Projects, Prediction models, Training and workshops or Full-day training');
 }
-const Services=userServices;
 
 let userRegion = prompt('Please write your Region. Options are: North America, South America, Europe, Oceania and Africa');
 while (userRegion === "" || !['north america', 'south america', 'europe', 'oceania', 'africa'].includes(userRegion.toLowerCase())) {
     userRegion = prompt('Please write a correct Region. Options are: North America, South America, Europe, Oceania or Africa');
 }
-const Region = userRegion;
 
-function servicesPricing (Services) {
-    switch(Services) {
-        case 'data analysis projects':
-        return 800;
-        break;
-        case 'visualization projects':
-        return 600;
-        case 'prediction models':
-        return 700;
-        break;
-        case 'training and workshops':
-        return 900;
-        break;
-        case 'full-day training':
-        return 1000;
-        break;
-        default:
-            return 0;
-    }
+
+class Quote {
+    constructor(Services, Region, Pricing) {
+        this.Services = Services;
+        this.Region = Region;
+        this.Pricing = parseFloat(Pricing);
+    } 
 }
 
-function regionPricing (Region, basePrice) {
-    switch(Region) {
-        case 'north america':
-            return basePrice+100;
-        break;
-        case 'south america':
-            return basePrice+50;
-        case 'europe':
-            return basePrice+150;
-        break;
-        case 'oceania':
-            return basePrice+50;
-        break;
-        case 'africa':
-            return basePrice+100;
-        break;
-        default:
-            return basePrice;
-    }
+const Quotes = [];
+Quotes.push(new Quote("data analysis projects", "north america", 900));
+Quotes.push(new Quote("data analysis projects", "south america", 850));
+Quotes.push(new Quote("data analysis projects", "europe", 950));
+Quotes.push(new Quote("data analysis projects", "oceania", 850));
+Quotes.push(new Quote("data analysis projects", "africa", 900));
+Quotes.push(new Quote("visualization projects", "north america", 700));
+Quotes.push(new Quote("visualization projects", "south america", 650));
+Quotes.push(new Quote("visualization projects", "europe", 750));
+Quotes.push(new Quote("visualization projects", "oceania", 650));
+Quotes.push(new Quote("visualization projects", "africa", 700));
+Quotes.push(new Quote("prediction models", "north america", 800));
+Quotes.push(new Quote("prediction models", "south america", 750));
+Quotes.push(new Quote("prediction models", "europe", 850));
+Quotes.push(new Quote("prediction models", "oceania", 750));
+Quotes.push(new Quote("prediction models", "africa", 800));
+Quotes.push(new Quote("training and workshops", "north america", 1000));
+Quotes.push(new Quote("training and workshops", "south america", 950));
+Quotes.push(new Quote("training and workshops", "europe", 1050));
+Quotes.push(new Quote("training and workshops", "oceania", 950));
+Quotes.push(new Quote("training and workshops", "africa", 1000));
+Quotes.push(new Quote("full-day training", "north america", 1100));
+Quotes.push(new Quote("full-day training", "south america", 1050));
+Quotes.push(new Quote("full-day training", "europe", 1150));
+Quotes.push(new Quote("full-day training", "oceania", 1050));
+Quotes.push(new Quote("full-day training", "africa", 1100));
 
+const matchingQuote = Quotes.find(quote => quote.Services === userServices.toLowerCase() && quote.Region === userRegion.toLowerCase());
+
+if (matchingQuote) {
+    alert("User Information\nName: " + Name + "\nLast name: " + LastName + "\nEmail: " + Email + "\nQuote Details\nService: " + matchingQuote.Services + "\nRegion: " + matchingQuote.Region + "\nPricing: " + matchingQuote.Pricing);
+} else{
+    alert("ERROR. Run this program again");
 }
-
-const basePrice=servicesPricing(Services);
-const pricing = regionPricing(Region, basePrice);
-const quote = new Quote (Services,Region,pricing);
-console.log("Quote:", quote);
-
-alert("Name: "+Name+"\nLast name: "+LastName+"\nEmail: "+Email+"\nQuote Details.\nService: "+quote.Services+"\nRegion: "+quote.Region+"\nPricing: "+quote.pricing);
